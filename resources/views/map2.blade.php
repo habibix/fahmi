16e75845fab4fa264f189a4161737237b55d64c2
habibix <habibi@gmail.com>
2019-04-08 16:53:13 +0700

    add bae

--- resources/views/map2.blade.php
+++ resources/views/map2.blade.php
@@ -1,54 +1,79 @@
-<!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Rectangles</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 425px;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-    <script>
+<head>
+<title>jQuery add / remove textbox example</title>
+ 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
-      // This example adds a red rectangle to a map.
+<style type="text/css">
+  div{
+    padding:8px;
+  }
+</style>
 
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 20,
-          center: {lat: -6.8585, lng: 107.42},
-          mapTypeId: 'terrain'
-        });
+</head>
 
-        var rectangle = new google.maps.Rectangle({
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.05,
-          map: map,
-          bounds: {
-            north: -6.836, //LINTANG A
-            south: -6.881, //LINTANG B
-            east: 107.443, //BUJUR A
-            west: 107.397 //BUJUR B
-          }
-        });
-      }
-    </script>
-  </head>
-  <body>
-    <div id="map"></div>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZGCoJLniH-3xUOaBlX2aKrkG6KNeRecM&callback=initMap">
-    </script>
-  </body>
+<body>
+
+<h1>jQuery add / remove textbox example</h1>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+    var counter = 2;
+    
+    $("#addButton").click(function () {
+        
+  if(counter>10){
+            alert("Only 10 textboxes allow");
+            return false;
+  }   
+    
+  var newTextBoxDiv = $(document.createElement('div'))
+       .attr("id", 'TextBoxDiv' + counter);
+                
+  newTextBoxDiv.after().html('<label>Textbox #'+ counter + ' : </label>' +
+        '<input type="text" name="textbox' + counter + 
+        '" id="textbox' + counter + '" value="" >');
+            
+  newTextBoxDiv.appendTo("#TextBoxesGroup");
+
+        
+  counter++;
+     });
+
+     $("#removeButton").click(function () {
+  if(counter==1){
+          alert("No more textbox to remove");
+          return false;
+       }   
+        
+  counter--;
+      
+        $("#TextBoxDiv" + counter).remove();
+      
+     });
+    
+     $("#getButtonValue").click(function () {
+    
+  var msg = '';
+  for(i=1; i<counter; i++){
+      msg += "\n Textbox #" + i + " : " + $('#textbox' + i).val();
+  }
+        alert(msg);
+     });
+  });
+</script>
+</head><body>
+
+<div id='TextBoxesGroup'>
+  <div id="TextBoxDiv1">
+    <label>Textbox #1 : </label><input type='textbox' id='textbox1' >
+  </div>
+</div>
+<input type='button' value='Add Button' id='addButton'>
+<input type='button' value='Remove Button' id='removeButton'>
+<input type='button' value='Get TextBox Value' id='getButtonValue'>
+
+</body>
 </html>
